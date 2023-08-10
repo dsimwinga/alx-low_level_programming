14e3c35affeb9b41e9b6852b4b9e1f596f79e98e
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-10 05:39:03 -0700

    printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is my main function
+ *
+ * Return: always return 0 when successeful
+ */
+
+int main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
