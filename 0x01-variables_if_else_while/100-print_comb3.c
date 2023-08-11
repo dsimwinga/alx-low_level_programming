e3b6e2c333b033d87257c7feb4a06043610b4f26
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-11 15:53:50 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - my main function
+ *
+ * Return: return 0 at success
+ */
+
+int main(void)
+{
+
+int b = '0';
+int c = '0';
+
+for (c = '0' ; c <= '9' ; c++)
+{
+	for (b = '0' ; b <= '9' ; b++)
+	{
+		if (!((b == c) || (c > b)))
+		{
+		putchar(c);
+		putchar(b);
+		if (!(b == 9 && c == 8))
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+}
+
+
+putchar('\n');
+	return (0);
+}
