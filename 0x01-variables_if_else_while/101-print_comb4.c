819be9e592e358c25d8349f6732373c5aebc5e50
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-11 16:10:57 -0700

    print combo4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - my main function
+ *
+ * Return: return 0 at success
+ */
+
+int main(void)
+{
+
+	int a = '0';
+	int b = '0';
+	int c = '0';
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		for (b = '0' ; b <= '9' ; b++)
+		{
+			for (a = '0' ; a <= '9' ; a++)
+			{
+				if (!((a == b) || (b == c) || (b > a) || (c > b)))
+				{
+					putchar(c);
+					putchar(b);
+					putchar(a);
+					if (!(a == '9' && c == '7' && b == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+putchar('\n');
+return (0);
+}
