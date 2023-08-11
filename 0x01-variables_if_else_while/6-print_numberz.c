249c52e532cfdd6f0f32f31ad85de19177aa7ce8
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-11 13:40:32 -0700

    printing numberz in base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - my main function
+ *
+ * Return: return 0 at success
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+
+return (0);
+}
