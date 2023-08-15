77a587a7ce38f539ccb8b94ce27ecc945f5980fa
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-15 21:27:49 +0200

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - my main function
+ *
+ * Return: return 0 at success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
