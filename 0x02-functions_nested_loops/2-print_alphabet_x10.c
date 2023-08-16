4d71bea8d2fb524c3150da2e81f782c8cbe966f0
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-16 11:48:54 +0200

    printing alphabet using neste for loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print alphabet in lowercase
+*
+*Return: return 0;
+*/
+
+void print_alphabet_x10(void)
+{
+int j;
+char i;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar(i);
+	}
+_putchar('\n');
+}
