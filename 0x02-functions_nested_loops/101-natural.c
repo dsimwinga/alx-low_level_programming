eb944a2858a0a547c2e2b4943fe3427887f91ac5
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-16 14:33:51 +0200

    fibonacci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - my main function
+*
+* Return: returns 0;
+*/
+	int main(void)
+	{
+		int i, sum = 0;
+
+
+		for (i = 0; i < 1024; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
+				sum += i;
+		}
+
+
+		printf("%d\n", sum);
+
+
+		return (0);
+	}
