dff14467954a0063a2685566d6800eb62410eca6
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-16 13:16:44 +0200

    computing absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - prints an absolute value
+ *@f: checks for a positive value
+ *Return: return 0;
+ */
+
+int _abs(int f)
+{
+
+if (f < 0)
+	return (-f);
+else
+	return (f);
+}
