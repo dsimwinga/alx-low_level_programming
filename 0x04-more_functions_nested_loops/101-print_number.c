b20738a41dcafe921a0354fdb2ee66b87f48d066
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-17 22:36:36 +0200

    printing an integer number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_number - my main function
+ *@n: prints an integer
+ *Return: return 0 at success
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
