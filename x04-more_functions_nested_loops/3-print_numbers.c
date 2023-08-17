40e2f388c3967bf1bb76d1ba0b010df2ee0f0d6c
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-17 12:47:56 +0200

    through 9

---
+++ x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - my main function
+ *Return: returns 0 at success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+for (i = 0 ; i < 10 ; i++)
+	_putchar(i + '0');
+_putchar('\n');
+
+}
