b31bd9bebf65b56cd86934eb1a121f85a1bfad82
dsimwinga <infor.dsimwinga@gmail.com>
2023-08-21 22:32:21 +0200

    a program that generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+
+/**
+ *main - my main function
+ *Return: 0 at success
+ *
+ *
+ */
+
+int main(void)
+{
+	int pin[100];
+	int g, add, m;
+
+	add = 0;
+
+	srand(time(NULL));
+
+	for (g = 0 ; g < 100 ; g++)
+	{
+		pin[g] = rand() % 78;
+		add += (pin[g] + '0');
+		putchar(pin[g] + '0');
+		if ((2772 - add) - '0' < 78)
+		{
+			m = 2772 - add - '0';
+			add += m;
+			putchar(m + '0');
+			break;
+		}
+
+	}
+return (0);
+}
